55c1db156571daf3e1ba7d1bf76e82bf85900054
maharong <kija4545@naver.com>
2025-07-22 04:42:25 +0900

    Initial commit

---
+++ src/main/java/com/github/maharong/economycore/EconomyCore.java
@@ -0,0 +1,17 @@
+package com.github.maharong.economycore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EconomyCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
